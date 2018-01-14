71e1269990eaf2a31e809982b272402d6299edf5
HARJEET SINGH <harrysbhatti22@gmail.com>
2018-01-14 11:10:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log(string.toUpperCase())}
 function logWhisper(string){console.log(string.toLowerCase())}
-function sayHiToGrandama(string){if(string.toLowerCase===string)
- { return "I can't hear you!"}
+function sayHiToGrandama(string){var a="I can't hear you!";
+  if(string.toLowerCase===string)
+ { return a}
 }
 function sayHiToGrandma(string){if(string.toUpperCase===string)
   {return "YES INDEED!"}
